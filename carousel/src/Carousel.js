bb39fe83737c66e1adad115330c99c8e1b7b4205
mvmxzy <mimozathuynh@gmail.com>
2023-11-05 14:15:48 -0500

    Coins

---
+++ carousel/src/Carousel.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./Carousel.css";
+import Card from "./Card";
+
+
+
+ function Carousel({ photos, title }) {
+  const [currCardIdx, setCurrCardIdx] = useState(0);
+
+  const currCard = photos[currCardIdx];
+  const total = photos.length;
+
+  function goForward() {
+    setCurrCardIdx(currCardIdx + 1);
+  }
+
+  return (
+    <div className="Carousel">
+      <h1>{title}</h1>
+      <div className="Carousel-main">
+        <i
+          className="bi bi-arrow-left-circle"
+          onClick={goForward}
+        />
+        <Card
+          caption={currCard.caption}
+          src={currCard.src}
+          currNum={currCardIdx + 1}
+          totalNum={total}
+        />
+        <i
+          className="bi bi-arrow-right-circle"
+          onClick={goForward}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
---
+++ carousel/src/Carousel.test.js
@@ -0,0 +1,28 @@
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import TEST_IMAGES from "./_testCommon.js";
+
+it("works when you click on the right arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
